6e84490d259e169899ec3550e444cb72ae566b4b
yared solomon <yaredyaya16@gmail.com>
2022-03-22 23:14:10 +0300

    registered all the repositories to the program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,45 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using WeSafe.Data;
+using WeSafe.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// builder.Services.AddControllers();
 builder.Services.AddControllers();
+
+//    var appSettingsSection = Configuration.GetSection("AppSettings");
+//     services.Configure<AppSettings>(appSettingsSection);
+
+// configure jwt authentication
+// var appSettings = appSettingsSection.Get<AppSettings>();
+
+
+var key = Encoding.ASCII.GetBytes("THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING");
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddScoped<IRepository<Person>, PersonRepository>();
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+builder.Services.AddScoped<IRepository<PoliceStation>, PoliceStationRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
