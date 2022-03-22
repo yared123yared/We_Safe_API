1e9c7aebc1256678a59d26c6e479fd52148c8d57
yared solomon <yaredyaya16@gmail.com>
2022-03-23 00:53:59 +0300

    added a connection string

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
+// using System.Configuration;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using WeSafe.Data;
 using WeSafe.Models;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(
+                builder.Configuration.GetConnectionString("wesafeConnection"),
+                 o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
 
+                ));
 // builder.Services.AddControllers();
 builder.Services.AddControllers();
 
@@ -39,7 +47,7 @@ builder.Services.AddAuthentication(x =>
 builder.Services.AddScoped<IRepository<Person>, PersonRepository>();
 builder.Services.AddScoped<IRepository<User>, UserRepository>();
 builder.Services.AddScoped<IRepository<PoliceStation>, PoliceStationRepository>();
-
+builder.Services.AddScoped<IRepository<Police>, PoliceRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
