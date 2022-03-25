079dafb4e52f1aefe2f864ab5404d6f7cf403fc4
yared solomon <yaredyaya16@gmail.com>
2022-03-25 03:20:11 +0300

    added a profile for role

---
+++ Profile/roleProfile.cs
@@ -0,0 +1,27 @@
+using WeSafe.DTO;
+using WeSafe.Models;
+
+using AutoMapper;
+namespace WeSafe.Profiles
+{
+
+
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<Role, RoleDto>()
+            .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId))
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.RoleName));
+            // .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
+            
+
+            CreateMap<RoleDto, Role>();
+
+        }
+
+
+
+    }
+
+}
