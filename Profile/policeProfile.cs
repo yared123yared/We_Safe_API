3f723649036f9f12a06ae292bca1047126156f38
yared solomon <yaredyaya16@gmail.com>
2022-03-26 15:22:07 +0300

    updated the police DTO

--- Profile/policeProfile.cs
+++ Profile/policeProfile.cs
@@ -14,10 +14,10 @@ namespace  WeSafe.Profiles
             .ForMember(dest => dest.PoliceId, opt => opt.MapFrom(src => src.PoliceId))
             .ForMember(dest => dest.Experiance, opt => opt.MapFrom(src => src.Experiance))
             .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Level))
-            .ForMember(dest => dest.Person, opt => opt.MapFrom(src => src.Person)) 
+            .ForMember(dest => dest.Person, opt => opt.MapFrom(src => src.Person));
             // .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
             // .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
-            .ForMember(dest => dest.Station, opt => opt.MapFrom(src => src.Station));
+            // .ForMember(dest => dest.Station, opt => opt.MapFrom(src => src.Station));
          
             
             CreateMap<PoliceDto, Police>();
