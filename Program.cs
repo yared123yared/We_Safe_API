9bfdbc2509aebad73d246e7db67fd0d42336a526
yared solomon <yaredyaya16@gmail.com>
2022-03-27 00:37:27 +0300

    added cors policy

--- Program.cs
+++ Program.cs
@@ -73,7 +73,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+ app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
