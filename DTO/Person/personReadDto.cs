0fdee0f6ac0fe0d8135ba59c09679522afe003d3
yared solomon <yaredyaya16@gmail.com>
2022-04-03 11:58:03 +0300

    fixed mapping issue

--- DTO/Person/personReadDto.cs
+++ DTO/Person/personReadDto.cs
@@ -1,30 +1,30 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using WeSafe.Models;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations.Schema;
+// using WeSafe.Models;
 
-namespace WeSafe.DTO
-{
+// namespace WeSafe.DTO
+// {
 
-    public class PersonReadDto
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int PersonId { get; set; }
+//     public class PersonReadDto
+//     {
+//         [Key]
+//         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+//         public int PersonId { get; set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Password { get; set; }
-        public string Phone { get; set; }
-        public string Picture { get; set; }
-        public string Sex { get; set; }
+//         public string FirstName { get; set; }
+//         public string LastName { get; set; }
+//         public string Password { get; set; }
+//         public string Phone { get; set; }
+//         public string Picture { get; set; }
+//         public string Sex { get; set; }
 
-        // Navigational Element
-        public Address Address { get; set; }
-        public int RoleId { get; set; }
+//         // Navigational Element
+//         public Address Address { get; set; }
+//         public int RoleId { get; set; }
 
-        public Role Role { get; set; }
+//         public Role Role { get; set; }
 
-    }
+//     }
 
-}
+// }
--- DTO/User/userCreateDto.cs
+++ DTO/User/userCreateDto.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WeSafe.DTO.Create;
+
+using WeSafe.Models;
 // using WeSafe.Models;
 
 namespace WeSafe.DTO
