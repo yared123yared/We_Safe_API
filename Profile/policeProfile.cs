b38f53a3743743f59239e6ac345d33392dfaba4d
yared solomon <yaredyaya16@gmail.com>
2022-04-03 14:19:31 +0300

    No commit

--- Profile/policeProfile.cs
+++ Profile/policeProfile.cs
@@ -12,7 +12,7 @@ namespace  WeSafe.Profiles
         {
             CreateMap<Police, PoliceDto>()
             .ForMember(dest => dest.PoliceId, opt => opt.MapFrom(src => src.PoliceId))
-            .ForMember(dest => dest.Experiance, opt => opt.MapFrom(src => src.Experiance))
+            // .ForMember(dest => dest.Experiance, opt => opt.MapFrom(src => src.Experiance))
             .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Level))
             .ForMember(dest => dest.Person, opt => opt.MapFrom(src => src.Person));
             // .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
