5cbce411a9901a1316b2a73a5bb82702c2649a47
yared solomon <yaredyaya16@gmail.com>
2022-04-03 14:26:40 +0300

    remvoed profile

--- Profile/policeProfile.cs
+++ Profile/policeProfile.cs
@@ -1,32 +1,32 @@
-using WeSafe.DTO;
-using WeSafe.Models;
+// using WeSafe.DTO;
+// using WeSafe.Models;
 
-using AutoMapper;
-namespace  WeSafe.Profiles
-{
+// using AutoMapper;
+// namespace  WeSafe.Profiles
+// {
     
 
-    public class PoliceProfile : Profile
-    {
-        public PoliceProfile()
-        {
-            CreateMap<Police, PoliceDto>()
-            .ForMember(dest => dest.PoliceId, opt => opt.MapFrom(src => src.PoliceId))
-            // .ForMember(dest => dest.Experiance, opt => opt.MapFrom(src => src.Experiance))
-            .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Level))
-            .ForMember(dest => dest.Person, opt => opt.MapFrom(src => src.Person));
-            // .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
-            // .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
-            // .ForMember(dest => dest.Station, opt => opt.MapFrom(src => src.Station));
+//     public class PoliceProfile : Profile
+//     {
+//         public PoliceProfile()
+//         {
+//             CreateMap<Police, PoliceDto>()
+//             .ForMember(dest => dest.PoliceId, opt => opt.MapFrom(src => src.PoliceId))
+//             .ForMember(dest => dest.Experiance, opt => opt.MapFrom(src => src.Experiance))
+//             .ForMember(dest => dest.Level, opt => opt.MapFrom(src => src.Level))
+//             .ForMember(dest => dest.Person, opt => opt.MapFrom(src => src.Person));
+//             // .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+//             // .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+//             // .ForMember(dest => dest.Station, opt => opt.MapFrom(src => src.Station));
          
             
-            CreateMap<PoliceDto, Police>();
+//             CreateMap<PoliceDto, Police>();
 
   
 
 
 
-    }
+//     }
 
-}
-}
+// }
+// }
