6a32266b161d3026274b14f73444908b20382c2d
yared solomon <yaredyaya16@gmail.com>
2022-05-14 14:05:19 +0300

    changed the assignedPoliceId to nullable

--- Models/case.cs
+++ Models/case.cs
@@ -11,7 +11,7 @@ namespace WeSafe.Models
         public int Id { get; set; }
         public DateTime OpenedDate { get; set; }
         public DateTime ClosedDate { get; set; }
-        public int AssignedPoliceId { get; set; }
+        public int? AssignedPoliceId { get; set; }
         public Police? AssignedPolice { get; set; }
         public int ReporterAdminId { get; set; }
         public Person? ReporterAdmin { get; set; }
