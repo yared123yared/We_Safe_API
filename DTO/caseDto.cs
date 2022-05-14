e4b091fa3ae8ffbb11fa6a71941bbdea02169d7a
yared solomon <yaredyaya16@gmail.com>
2022-05-14 14:00:51 +0300

    added a nullabel to the assigned police Id

--- DTO/caseDto.cs
+++ DTO/caseDto.cs
@@ -10,7 +10,7 @@ namespace WeSafe.DTO
        public int Id { get; set; }
         public DateTime? OpenedDate { get; set; }
         public DateTime? ClosedDate { get; set; }
-        public int 	AssignedPoliceId { get; set; }
+        public int? 	AssignedPoliceId { get; set; }
         public Police? AssignedPolice { get; set; }
 
         public int ReporterAdminId { get; set; }
